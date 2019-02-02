968bc100e64cb32f387157c9e1f367263cc8be36
Michael Rashkovetsky <misharash@gmail.com>
2019-02-02 11:09:29 +0200

    GNU GPL v3 license added

--- dump.cpp
+++ dump.cpp
@@ -1,3 +1,24 @@
+/*
+    Copyright 2019 Mykhailo Rashkovetskyi
+
+    This file is part of psr-distribution-test - a program to test
+    pulsar distribution using Monte-Carlo method.
+
+    psr-distribution-test is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    psr-distribution-test is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with psr-distribution-test. If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
 //Data output into file
 #include <cstdio>
 #include <cstdlib>
